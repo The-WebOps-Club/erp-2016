7c11d2a5705fa85b62870aa7299afe8f775f1e27
Deepak Padamata <deepakpadamata@gmail.com>
2015-04-15 15:40:00 +0530

    Upload perfect, download broken

--- server/api/upload/index.js
+++ server/api/upload/index.js
@@ -5,6 +5,8 @@ var controller = require('./upload.controller');
 
 var router = express.Router();
 
+
+router.get('/*', controller.serve);
 router.post('/', controller.create);
 
 module.exports = router;
