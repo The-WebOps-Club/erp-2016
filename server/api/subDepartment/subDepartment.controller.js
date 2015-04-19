62f5a329c8fc5b9caf80e9f7fc3b93aaf025a8c5
chinnichaitanya <chchaitanya95@gmail.com>
2015-04-19 11:31:43 +0530

    security hole-bug fix in signup

--- server/api/subDepartment/subDepartment.controller.js
+++ server/api/subDepartment/subDepartment.controller.js
@@ -17,18 +17,18 @@ exports.index = function(req, res) {
 exports.show = function(req, res) {
   SubDepartment.findById(req.params.id, function (err, subDepartment) {
     if(err) { return handleError(res, err); }
-    if(!subDepartment) { return res.send(404); }
+    if(!subDepartment) { return res.sendStatus(404); }
     return res.json(subDepartment);
   });
 };
 
 // Creates a new subDepartment in the DB.
 exports.create = function(req, res) {
-  SubDepartment.create(req.body, function(err, subDepartment) {
+  SubDepartment.create(req.body, function (err, subDepartment) {
     if(err) { return handleError(res, err); }
     Department.findById(req.body.department, function (err, department) {
       if(err) { return handleError(res, err); }
-      if(!department) { return res.send(404); }
+      if(!department) { return res.sendStatus(404); }
       if(department.subDepartments.indexOf(subDepartment._id) == -1) {
         department.subDepartments.push(subDepartment._id);
         department.save(function (err) {
@@ -45,7 +45,7 @@ exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
   SubDepartment.findById(req.params.id, function (err, subDepartment) {
     if (err) { return handleError(res, err); }
-    if(!subDepartment) { return res.send(404); }
+    if(!subDepartment) { return res.sendStatus(404); }
     var updated = _.merge(subDepartment, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
@@ -58,10 +58,10 @@ exports.update = function(req, res) {
 exports.destroy = function(req, res) {
   SubDepartment.findById(req.params.id, function (err, subDepartment) {
     if(err) { return handleError(res, err); }
-    if(!subDepartment) { return res.send(404); }
+    if(!subDepartment) { return res.sendStatus(404); }
     subDepartment.remove(function(err) {
       if(err) { return handleError(res, err); }
-      return res.send(204);
+      return res.sendStatus(204);
     });
   });
 };
