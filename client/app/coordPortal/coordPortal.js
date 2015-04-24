274f1d5aa0c52a5eebbb5d1b6b6365d000fda541
chinnichaitanya <chchaitanya95@gmail.com>
2015-04-25 05:20:21 +0530

    added frontend redirection

--- client/app/coordPortal/coordPortal.js
+++ client/app/coordPortal/coordPortal.js
@@ -7,64 +7,54 @@ angular.module('erp2015App')
         url: '/coordPortal/dashboard',
         templateUrl: 'app/coordPortal/dashboard/dashboard.html',
         controller: 'CoordPortalDashboardCtrl',
-        authenticate: true
-        // data: {
-        //   permissions: {
-        //       only: ['admin', 'user'],
-        //       redirectTo: 'coordPortalEditProfile'
-        //   }        
-        // }                
+        authenticate: true,
+        data: {
+          permissions: {
+              only: ['admin', 'user'],
+              redirectTo: 'coordPortalEditProfile'
+          }        
+        }                
       })
       .state('coordPortalCores', {
         url: '/coordPortal/admin',
         templateUrl: 'app/coordPortal/cores/cores.html',
         controller: 'CoordPortalCoresCtrl',
-        authenticate: true
-
-        // data: {
-        //   permissions: {
-        //       only: ['admin', 'core'],
-        //       redirectTo: 'coordPortalEditProfile'
-        //   }        
-        // }                
+        authenticate: true,
+        data: {
+          permissions: {
+              only: ['admin', 'core'],
+              redirectTo: 'coordPortalEditProfile'
+          }        
+        }                
       })
       .state('coordPortalResponses', {
         url: '/coordPortal/admin/submissions/:id',
         templateUrl: 'app/coordPortal/cores/responses.html',
         controller: 'CoordPortalResponsesCtrl',
-        authenticate: true
-
-        // data: {
-        //   permissions: {
-        //       only: ['admin', 'core'],
-        //       redirectTo: 'coordPortalEditProfile'
-        //   }        
-        // }                
+        authenticate: true,
+        data: {
+          permissions: {
+              only: ['admin', 'core'],
+              redirectTo: 'coordPortalEditProfile'
+          }        
+        }                
       })
       .state('coordPortalResponseDetails', {
         url: '/coordPortal/admin/application/:id',
         templateUrl: 'app/coordPortal/cores/responseDetails.html',
         controller: 'CoordPortalResponseDetailsCtrl',
-        authenticate: true
-
-        // data: {
-        //   permissions: {
-        //       only: ['admin', 'core'],
-        //       redirectTo: 'coordPortalEditProfile'
-        //   }        
-        // }                      
+        authenticate: true,
+        data: {
+          permissions: {
+              only: ['admin', 'core'],
+              redirectTo: 'coordPortalEditProfile'
+          }        
+        }                      
       })
       .state('coordPortalEditProfile', {
         url: '/coordPortal/editProfile',
         templateUrl: 'app/coordPortal/editProfile/editProfile.html',
         controller: 'CoordPortalEditProfileCtrl',
         authenticate: true
-
-        // data: {
-        //   permissions: {
-        //       except: ['anonymous'],
-        //       redirectTo: 'login'
-        //   }        
-        // }                
       });      
   });
