b9f8c4c2c49566d0b77f91b7a1182b4590085f0a
chinnichaitanya <chchaitanya95@gmail.com>
2015-04-25 11:53:31 +0530

    add more mime types for zip

--- server/api/upload/upload.controller.js
+++ server/api/upload/upload.controller.js
@@ -10,7 +10,7 @@ var gfs = new Grid(mongoose.connection.db);
 
 exports.create = function (req, res) {
   var part = req.files.file;
-  if(part.mimetype != "application/zip" && part.mimetype != "application/octet-stream" && part.mimetype != "application/x-zip-compressed") {
+  if(part.mimetype != "application/zip" && part.mimetype != "application/octet-stream" && part.mimetype != "application/x-zip-compressed" && part.mimetype != "application/x-zip") {
     res.send({
       message: 'Please upload zip files only'
     })
