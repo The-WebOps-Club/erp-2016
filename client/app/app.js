b30382c98a6d96ae8dc231baeccf5b2db9cc06ef
Deepak Padamata <deepakpadamata@gmail.com>
2015-05-30 11:24:57 +0530

    Add angular material

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('erp2015App', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngMaterial',
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
