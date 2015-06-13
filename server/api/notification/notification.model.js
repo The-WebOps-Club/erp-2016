ea6695f48242bfa32bd49179cf52f9fb565a7559
Deepak Padamata <deepakpadamata@gmail.com>
2015-06-13 09:39:03 +0530

    Modified notification model

--- server/api/notification/notification.model.js
+++ server/api/notification/notification.model.js
@@ -3,9 +3,19 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+//post: ID of the post being notified to
+//user: User to whom the notification belongs
+//postedBy, commentedBy: Users who posted or commented
+//action: 'post' or 'comment' to show which action has been done
+
 var NotificationSchema = new Schema({
   name: String,
   info: String,
+  action: String,
+  post: { type: Schema.Types.ObjectId, ref: 'Post' },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  postedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+  commentedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   active: Boolean
 });
 
