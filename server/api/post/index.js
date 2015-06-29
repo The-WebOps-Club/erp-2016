51c9d53851d399cd4ba99f8c3688942d16997801
Deepak Padamata <deepakpadamata@gmail.com>
2015-06-29 22:40:06 +0530

    Added comment deep populate

--- server/api/post/index.js
+++ server/api/post/index.js
@@ -12,8 +12,11 @@ router.get('/:id/:page', auth.isAuthenticated(), controller.index);
 
 router.post('/', auth.isAuthenticated(), controller.createPost);
 router.post('/newsfeed/refresh/', auth.isAuthenticated(), controller.newsfeedRefresh);
+router.post('/newsfeed/history/', auth.isAuthenticated(), controller.newsfeedHistory);
+router.post('/:id/refresh/', auth.isAuthenticated(), controller.refresh);
+router.post('/:id/history/', auth.isAuthenticated(), controller.history);
 router.post('/addComment', auth.isAuthenticated(), controller.addComment);
-router.post('/:id', auth.belongsTo(), controller.acknowledge); //What does this do?
+router.post('/:id', auth.isAuthenticated(), controller.acknowledge);
 
 router.put('/:id', auth.isAuthenticated(), controller.update);
 
