b39511549bc4f109c60b4ad51da7f6c5027b0b89
Rahul <kejriwalrahul@outlook.com>
2015-07-06 11:26:28 +0530

    made display event details

--- client/app/eventsPortal/event/event.controller.js
+++ client/app/eventsPortal/event/event.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('erp2015App')
-  .controller('EventsPortalEventCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('EventsPortalEventCtrl', function ($scope,$http,EventsPortalService) {
+    
+    var id = location.pathname.split('/')[3];
+ 	EventsPortalService.getEvent(id).then(function (event){
+ 		$scope.event = event;
+ 		$scope.eventTabs = event.eventTabs;
+ 		console.log(event);
+ 	});
+
+
   });
