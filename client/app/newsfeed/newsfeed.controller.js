78ea77f031842b8bdf15c66f7565c59dddc25c52
vignesh-m <vicky.mano@gmail.com>
2015-07-09 13:07:39 +0530

    newsfeed controller working

--- client/app/newsfeed/newsfeed.controller.js
+++ client/app/newsfeed/newsfeed.controller.js
@@ -1,32 +1,34 @@
 'use strict';
 
 angular.module('erp2015App')
-  .controller('NewsfeedCtrl', function ($scope, $http, $stateParams, $state, socket, Auth, postComment) {
+  .controller('NewsfeedCtrl', function ($scope, $http, $stateParams, $state, socket, Auth, postComment, postService) {
     $scope.newPost = '';
     $scope.newPostTitle = '';
     $scope.posts = {};
 	
-	$http.get('/api/posts/newsfeed/')
-		.success(function(posts) {
-    		$scope.posts = posts;
-            socket.syncUpdates('post', $scope.posts);			
-        })
-        .error(function(err) {
-            /*
-            Do some error handling here
-             */
-            console.log(err);
-            $state.go('404');
-		});
-
-    $scope.addComment = function(post) {
-        postComment.addComment(post._id, post.newComment)
-            .success(function(data) {
-                $scope.newPost = '';
-                $scope.newPostTitle = '';
-            })
-            .error(function(err) {
-                console.log(err);
-            })
-    }
+	// $http.get('/api/posts/newsfeed/1')
+	// 	.success(function(posts) {
+ //    		$scope.posts = posts;
+ //            socket.syncUpdates('post', $scope.posts);			
+ //        })
+ //        .error(function(err) {
+ //            /*
+ //            Do some error handling here
+ //             */
+ //            console.log(err);
+ //            $state.go('404');
+	// 	});
+    postService.getNewsFeed(1).then(function(data){
+        $scope.posts=data;
+    })
+    // $scope.addComment = function(post) {
+    //     postComment.addComment(post._id, post.newComment)
+    //         .success(function(data) {
+    //             $scope.newPost = '';
+    //             $scope.newPostTitle = '';
+    //         })
+    //         .error(function(err) {
+    //             console.log(err);
+    //         })
+    // }
   });
--- client/app/postService/postService.service.js
+++ client/app/postService/postService.service.js
@@ -4,23 +4,23 @@ angular.module('erp2015App')
   .service('postService', function ($http) {
     return {
     	addComment: function(id,comment){
-    		$http.post('/posts/addComment',
+    		return $http.post('/api/posts/addComment',
     			{postId:id,comment:comment}).then(function(response){
     			return response.data;
     		})
     	},
     	getPosts: function(id,page){
-    		$http.get('/posts/'+id+'/'+page).then(function(response){
+    		return $http.get('/api/posts/'+id+'/'+page).then(function(response){
     			return response.data;
     		})
     	},
     	getNewsFeed: function(page){
-			$http.get('/posts/newsfeed/'+page).then(function(response){
+			return $http.get('/api/posts/newsfeed/'+page).then(function(response){
     			return response.data;
     		})
     	},
     	acknowledge: function(id){
-    		$http.post('/posts/'+id).then(function(response){
+    		return $http.post('/api/posts/'+id).then(function(response){
     			return response.data;
     		})
     	}
