6797b609b35503d30a790254cb664964268a6baa
vignesh-m <vicky.mano@gmail.com>
2015-07-09 13:27:10 +0530

    added comment support

--- client/app/newsfeed/newsfeed.controller.js
+++ client/app/newsfeed/newsfeed.controller.js
@@ -18,17 +18,19 @@ angular.module('erp2015App')
  //            console.log(err);
  //            $state.go('404');
 	// 	});
-    postService.getNewsFeed(1).then(function(data){
-        $scope.posts=data;
-    })
-    // $scope.addComment = function(post) {
-    //     postComment.addComment(post._id, post.newComment)
-    //         .success(function(data) {
-    //             $scope.newPost = '';
-    //             $scope.newPostTitle = '';
-    //         })
-    //         .error(function(err) {
-    //             console.log(err);
-    //         })
-    // }
+    $scope.updateFeed=function(){
+        postService.getNewsFeed($scope.page).then(function(data){
+            $scope.posts=data;
+        })
+    }
+    $scope.page=1;
+    $scope.updateFeed();
+    $scope.addComment = function(post) {
+        postService.addComment(post._id,post.newComment).then(function(data){
+            var idx=$scope.posts.indexOf(post)
+            $scope.posts[idx]=data
+            $scope.posts[idx]['modified']=true
+            console.log(idx)
+        })
+    }
   });
