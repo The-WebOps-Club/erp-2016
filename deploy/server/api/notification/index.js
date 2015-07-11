e02a3cde26589ef5a25301a4b6b8660269ae7e31
Deepak Padamata <deepakpadamata@gmail.com>
2015-07-11 15:25:52 +0530

    notif populate to deploy

--- deploy/server/api/notification/index.js
+++ deploy/server/api/notification/index.js
@@ -7,8 +7,10 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
+router.post('/refresh', auth.isAuthenticated(), controller.refresh);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
+router.get('/:id/seen', controller.markAsSeen);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
