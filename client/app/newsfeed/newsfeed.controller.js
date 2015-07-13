5b21b84170583f77319cb2d79cc64bc758a4d236
vignesh-m <vicky.mano@gmail.com>
2015-07-13 12:47:11 +0530

    removed postcomment service

--- client/app/newsfeed/newsfeed.controller.js
+++ client/app/newsfeed/newsfeed.controller.js
@@ -1,23 +1,11 @@
 'use strict';
 
 angular.module('erp2015App')
-  .controller('NewsfeedCtrl', function ($scope, $http, $stateParams, $state, socket, Auth, postComment, postService) {
+  .controller('NewsfeedCtrl', function ($scope, $http, $stateParams, $state, socket, Auth, postService) {
     $scope.newPost = '';
     $scope.newPostTitle = '';
     $scope.posts = {};
 	
-	// $http.get('/api/posts/newsfeed/1')
-	// 	.success(function(posts) {
- //    		$scope.posts = posts;
- //            socket.syncUpdates('post', $scope.posts);			
- //        })
- //        .error(function(err) {
- //            /*
- //            Do some error handling here
- //             */
- //            console.log(err);
- //            $state.go('404');
-	// 	});
     $scope.updateFeed=function(){
         postService.getNewsFeed($scope.page).then(function(data){
             $scope.posts=data;
