109c57c5254996725c5923dca289aff69e297f3d
badarsh2 <badarsh2@gmail.com>
2015-07-13 23:01:11 +0530

    Image cropping brought back

--- client/app/eventsPortal/cores/cores.controller.js
+++ client/app/eventsPortal/cores/cores.controller.js
@@ -10,6 +10,16 @@ angular.module('erp2015App')
 
     var uploadfile='';
 
+    var dataURItoBlob = function(dataURI) {
+        var binary = atob(dataURI.split(',')[1]);
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        var array = [];
+        for(var i = 0; i < binary.length; i++) {
+          array.push(binary.charCodeAt(i));
+        }
+        return new Blob([new Uint8Array(array)], {type: mimeString});
+      };
+
      var handleFileSelect=function(evt) {
         var myfile=evt.currentTarget.files[0];
         var reader = new FileReader();
@@ -35,7 +45,7 @@ angular.module('erp2015App')
       if(form.$valid) {
           $upload.upload({
                         url: 'api/uploads/',
-                        file: uploadfile
+                        file: dataURItoBlob($scope.myCroppedImage)
                     }).success(function (data, status, headers, config){
                       console.log('done');
                       console.log(uploadfile.name);
