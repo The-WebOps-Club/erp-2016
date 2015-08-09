ab01dad3ef6d1e5b2b01338e6e12e35c2934d326
chinnichaitanya <chchaitanya95@gmail.com>
2015-08-09 21:12:36 +0530

    indent code

--- client/app/eventsPortal/cores/cores.controller.js
+++ client/app/eventsPortal/cores/cores.controller.js
@@ -45,34 +45,35 @@ angular.module('erp2015App')
       console.log($scope.myCroppedImage)*/
       // $http.post('/api/uploads', {file: $scope.myCroppedImage});
       if(form.$valid) {
-          $upload.upload({
-                        url: 'api/uploads',
-                        file: $scope.myCroppedImage
-                    }).success(function (data, status, headers, config){
-                      console.log('done');
-                    });
+        $upload.upload({
+          url: 'api/uploads',
+          file: $scope.myCroppedImage
+        })
+        .success(function (data, status, headers, config) {
+          console.log('done');
+        });
 
-      		EventsPortalService.createEventList({
-        		title: $scope.eventList.title,
-        		info: $scope.eventList.info
-      		})
-      		.then(function (data) {
-            	$state.go('eventList');
-      		})
-      		.catch(function (err) {
-        		err = err.data;
-        		$scope.errors = {};
+    		EventsPortalService.createEventList({
+      		title: $scope.eventList.title,
+      		info: $scope.eventList.info
+    		})
+    		.then(function (data) {
+          	$state.go('eventList');
+    		})
+    		.catch(function (err) {
+      		err = err.data;
+      		$scope.errors = {};
 
-        		// Update validity of form fields that match the mongoose errors
-        		angular.forEach(err.errors, function (error, field) {
-          			form[field].$setValidity('mongoose', false);
-          			$scope.errors[field] = error.message;
-        		});
+      		// Update validity of form fields that match the mongoose errors
+      		angular.forEach(err.errors, function (error, field) {
+      			form[field].$setValidity('mongoose', false);
+      			$scope.errors[field] = error.message;
       		});
+    		});
     	}
   	};
 
-    $scope.createEvent = function(form) {
+    $scope.createEvent = function (form) {
       $scope.submitted = true;
       $scope.coordsIds = [];
       $scope.eventListIds = [];
@@ -84,43 +85,43 @@ angular.module('erp2015App')
       });
 
       if(form.$valid) {
-          EventsPortalService.createEvent({
-            name: $scope.event.title,
-            info: $scope.event.info,
-            assignees: $scope.coordsIds,
-            eventCategory: $scope.eventListIds
-          })
-          .then(function (data) {
-            $state.go('eventList');
-            // angular.forEach($scope.selectedEventLists, function (selectedEventList) {
-            //   selectedEventList.events.push(data);
-            //   $scope.eventIds = [];
-            //   angular.forEach(selectedEventList.events, function (item) {
-            //     $scope.eventIds.push(item._id);
-            //   });
-            //   EventsPortalService.editEventList({
-            //     _id: selectedEventList._id,
-            //     events: $scope.eventIds
-            //   })
-            //   .then(function (data){
-            //     console.log(data);
-            //     $state.go('eventList');
-            //   })
-            //   .catch(function (err){
-            //     console.log(err);
-            //   });  
-            // });
-          })
-          .catch(function (err) {
-            err = err.data;
-            $scope.errors = {};
+        EventsPortalService.createEvent({
+          name: $scope.event.title,
+          info: $scope.event.info,
+          assignees: $scope.coordsIds,
+          eventCategory: $scope.eventListIds
+        })
+        .then(function (data) {
+          $state.go('eventList');
+          // angular.forEach($scope.selectedEventLists, function (selectedEventList) {
+          //   selectedEventList.events.push(data);
+          //   $scope.eventIds = [];
+          //   angular.forEach(selectedEventList.events, function (item) {
+          //     $scope.eventIds.push(item._id);
+          //   });
+          //   EventsPortalService.editEventList({
+          //     _id: selectedEventList._id,
+          //     events: $scope.eventIds
+          //   })
+          //   .then(function (data){
+          //     console.log(data);
+          //     $state.go('eventList');
+          //   })
+          //   .catch(function (err){
+          //     console.log(err);
+          //   });  
+          // });
+        })
+        .catch(function (err) {
+          err = err.data;
+          $scope.errors = {};
 
-            // Update validity of form fields that match the mongoose errors
-            angular.forEach(err.errors, function (error, field) {
-                form[field].$setValidity('mongoose', false);
-                $scope.errors[field] = error.message;
-            });
+          // Update validity of form fields that match the mongoose errors
+          angular.forEach(err.errors, function (error, field) {
+              form[field].$setValidity('mongoose', false);
+              $scope.errors[field] = error.message;
           });
+        });
       }
     };
 });
