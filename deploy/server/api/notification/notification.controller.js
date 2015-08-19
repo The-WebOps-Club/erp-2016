103e2d668a43c4666b6b1adc92f8efae7b0389aa
Deepak Padamata <deepakpadamata@gmail.com>
2015-08-19 02:33:12 +0000

    Deploy

--- deploy/server/api/notification/notification.controller.js
+++ deploy/server/api/notification/notification.controller.js
@@ -10,12 +10,17 @@ var gcm = require('node-gcm');
 var mailer=require('../../components/mailer');
 var getMembers = require('../wall/wall.controller').getMembers;
 
+var NOTIFICATIONSPERPAGE = 10;
+
 // Get list of notifications
 exports.index = function(req, res) {
-  Notification.find({user: req.user._id}, '-user', function (err, notifications) {
-    if(err) { return handleError(res, err); }
-    var populated = [];
-    forEach(notifications, function(notification, index, arr) {
+  Notification.paginate({user: req.user._id}, req.params.page, NOTIFICATIONSPERPAGE, function(error, pageCount, paginatedResults, itemCount) {
+    if(error) {
+      return handleError(res, error);
+    }
+    else {
+      var populated = [];
+      forEach(paginatedResults, function(notification, index, arr) {
         var done = this.async();
         notification.deepPopulate('postedBy commentedBy', 'name', function (err, _notification) {
           _notification.deepPopulate('post', 'wall createdBy', function (err, _notif) {
@@ -26,29 +31,30 @@ exports.index = function(req, res) {
       }, function allDone (notAborted, arr) {
         res.status(200).send(populated);
       });
-  });
+    }
+  }, {sortBy: {updatedOn: 1}});
 };
 
 exports.refresh = function(req, res) {
   Notification.find({
     user: req.user._id, 
     updatedOn:{
-        $gt: req.body.date
-      }
-    }, function (err, notifications) {
+      $gt: req.body.date
+    }
+  }, function (err, notifications) {
     if(err) { return handleError(res, err); }
     var populated = [];
     forEach(notifications, function(notification, index, arr) {
-        var done = this.async();
-        notification.deepPopulate('postedBy commentedBy', 'name', function (err, _notification) {
-          _notification.deepPopulate('post', 'wall createdBy', function (err, _notif) {
-            populated.push(_notif);
-            done();
-          })
-        });
-      }, function allDone (notAborted, arr) {
-        res.status(200).send(populated);
+      var done = this.async();
+      notification.deepPopulate('postedBy commentedBy', 'name', function (err, _notification) {
+        _notification.deepPopulate('post', 'wall createdBy', function (err, _notif) {
+          populated.push(_notif);
+          done();
+        })
       });
+    }, function allDone (notAborted, arr) {
+      res.status(200).send(populated);
+    });
   });
 };
 
@@ -72,11 +78,11 @@ exports.create = function(req, res) {
         var message=notification.postedBy.name +" posted on "+notification.post.wall.name;
         notifier(message, notification.user.deviceId);
         mailer('[sarang-erp-2016] '+ message,notification.post.info,notification.user.email,notification.post._id,false,function cb(err,info){
-            if (err){
-              return res.json(501,err);
-  
-            }
-            return res.json(200,'Mail has been sent');
+          if (err){
+            return res.json(501,err);
+
+          }
+          return res.json(200,'Mail has been sent');
         })
       } else {
         var myComment = notification.post.comments.reverse()[0];
@@ -84,16 +90,16 @@ exports.create = function(req, res) {
         notification.post.createdBy.name+ " on the " + notification.post.wall.name+" wall";
         notifier(message, notification.user.deviceId);
         mailer(message+'[saarang-erp-2016]',myComment.info,notification.user.email,notification.post._id,true,function cb(err,info){
-            if (err){
-              return res.json(501,err);
-            }
-            else{
-              return res.json(201, notification);
-            }
+          if (err){
+            return res.json(501,err);
+          }
+          else{
+            return res.json(201, notification);
+          }
         });
       }
     });
-  });
+});
 };
 
 exports.notifyAll = function (postId, callback) {
@@ -117,51 +123,57 @@ exports.bulkCreate = function(data, callback) {
   forEach(data.members, function(member, index, arr) {
     var done = this.async();
     if (data.action=='post'){
-      Notification.create({post: data.post._id, user: member._id, action: data.action, postedBy: data.post.createdBy._id, updatedOn: Date.now()} , function (err, notification) {
-       if(err) { return handleError(res, err); }
-       console.log(notification);
-       Notification.findById(notification._id)
-       .deepPopulate('user post.wall postedBy.name commentedBy.name')
-       .exec( function (err, notification) {
-          if(!(data.post.createdBy._id === member._id)){
+      if(!(data.post.createdBy._id.toString() == member._id.toString())){
+        done();
+      }
+      else {
+        Notification.create({post: data.post._id, user: member._id, action: data.action, postedBy: data.post.createdBy._id, updatedOn: Date.now()} , function (err, notification) {
+         if(err) { return handleError(res, err); }
+         Notification.findById(notification._id)
+         .deepPopulate('user post.wall postedBy.name commentedBy.name')
+         .exec( function (err, notification) {
            if(notification.user.deviceId){
             deviceIds = deviceIds.concat(notification.user.deviceId);
-           }
-           emails.push(notification.user.email);
           }
-         done();
+          emails.push(notification.user.email);
+          done();
+        });
        });
-     });
+      }
     }
     else{
-      Notification.create({post: data.post._id, user: member._id, action: data.action, commentedBy: data.post.comments.reverse()[0].createdBy._id} , function (err, notification) {
-       if(err) { return handleError(res, err); }
-       console.log(notification);
-       Notification.findById(notification._id)
-       .deepPopulate('user post.wall postedBy.name commentedBy.name')
-       .exec( function (err, notification) {
-          if(!(data.post.comments.reverse()[0].createdBy._id=== member._id)){
+      if(!(data.post.comments.reverse()[0].createdBy._id.toString() == member._id.toString())){
+        done();
+      }
+      else {
+        Notification.create({post: data.post._id, user: member._id, action: data.action, commentedBy: data.post.comments.reverse()[0].createdBy._id, updatedOn: Date.now()} , function (err, notification) {
+         if(err) { return handleError(res, err); }
+         Notification.findById(notification._id)
+         .deepPopulate('user post.wall postedBy.name commentedBy.name')
+         .exec( function (err, notification) {
            if(notification.user.deviceId){
             deviceIds = deviceIds.concat(notification.user.deviceId);
-           }
-           emails.push(notification.user.email);
           }
-         done();
+          emails.push(notification.user.email);
+          done();
+        });
        });
-     });
+      }
     }
 
   }, function allDone (notAborted, arr) {
     if(data.post.comments.length === 0){
       var message=data.post.createdBy.name +" posted on "+data.post.wall.name;
+      var emailText = message + ":\n" + data.post.info;
       notifier(message, deviceIds);
-      mailer('[Saarang ERP] ' + data.post.title, data.post.info, emails, data.post._id, false);
+      mailer('[Saarang ERP] ' + data.post.title, emailText, emails, data.post._id, false);
     } else {
       var myComment=data.post.comments.reverse()[0];
       var message=myComment.createdBy.name +" comment on a post by "+
       data.post.createdBy.name+ " on the " + data.post.wall.name+" wall";
+      var emailText = message + ":\n" + myComment.info;
       notifier(message, deviceIds);
-      mailer('[Saarang ERP] ' + data.post.title ,myComment.info, emails, data.post._id, true);
+      mailer('[Saarang ERP] ' + data.post.title , emailText, emails, data.post._id, true);
     }
     callback();
   });
