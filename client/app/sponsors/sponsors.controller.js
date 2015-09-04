3ef1bc0e1d8a5c4c9cd15c20b4620bb0e568e5a8
vignesh-m <vicky.mano@gmail.com>
2015-09-04 17:26:33 +0530

    added spons form

--- client/app/sponsors/sponsors.controller.js
+++ client/app/sponsors/sponsors.controller.js
@@ -1,6 +1,49 @@
 'use strict';
 
 angular.module('erp2015App')
-  .controller('SponsorsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('SponsorsCtrl',function (Upload,$scope,$http) {
+    $scope.file=null
+    $scope.submit = function(){
+      if($scope.file && $scope.form.file.$valid && !$scope.file.$error){
+        $scope.upload($scope.file,function(err,data){
+          console.log(data)
+          if(err || !data.fileId){
+            console.log(err)
+            return;
+          }
+          var body={
+            title:$scope.title,
+            sponsor_link:$scope.sponsor_link,
+            logo:data.fileId,
+            priority:$scope.priority||10,
+            row_layout:$scope.row_layout||1,
+            active:$scope.active||true
+          }
+          $http.post('/api/sponsors',body).
+            then(function(response){
+              console.log(response.data)
+              //redirect to view spons
+            })
+          console.log(data)
+        })
+      } else {
+        console.log("error")
+        if($scope.form && $scope.form.file){
+          console.log($scope.form.file.$error)
+          console.log($scope.form.file.$valid)
+        }
+        if($scope.file)
+          console.log($scope.file.$error)
+      }
+    }
+    $scope.upload = function(file,cb){
+        Upload.upload({
+          url:'/api/uploads',
+          file:$scope.file
+        }).success(function(data){
+          cb(null,data)
+        }).error(function(err){
+          cb(err,null)
+        })
+    }
   });
